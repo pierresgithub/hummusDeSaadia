26308ccc8894b0b29e52c0ba223062b7b72590b7
pierresgithub <32385066+pierresgithub@users.noreply.github.com>
2017-10-26 16:20:37 +0200

    firstchange

---
+++ reciepes.php
@@ -0,0 +1,31 @@
+<?php  include('include/header.php')?>
+
+<section id="recettes">
+
+<div class="reciepes">
+            <h2>Recette d'afrique</h2>
+<!--  a href = le lien vers la page-->
+<a href="contry.php?reciepes=afrique"><img src="asset/images/afrique.jpg" alt="" class="imgRecettes"></a>
+</div>
+
+<div class="reciepes">
+            <h2>Recette d'asie</h2>
+            <a href="contry.php?reciepes=asie"><img src="asset/images/asie.jpeg" alt="" class="imgRecettes"></a>
+</div>
+
+<div class="reciepes">
+            <h2>Recette belge</h2>
+            <a href="contry.php?reciepes=belgique"><img src="asset/images/belgique.jpg" alt="" class="imgRecettes"></a>
+</div>
+
+<div class="reciepes">
+            <h2>Recette australienne</h2>
+            <a href="country.php?reciepes=australie"><img src="asset/images/australie.jpeg" alt="" class="imgRecettes"></a>
+</div>
+
+
+
+
+            </section>
+
+<?php  include('include/footer.php')?>
